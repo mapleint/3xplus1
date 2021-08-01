6e5ff71fc751c9296b14e18841aaab46bd40458d
mapleint <albertarlington21@gmail.com>
2021-08-01 11:02:11 +0000

    add file from disk
    I am somewhat proud of the speed, I am unsure how you could optimize this,

---
+++ 3np1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+inline unsigned long long n3p1 (unsigned long long  x)
+{
+	return x = x & 1 ? 3*x+1 : x >> 1;
+}
+
+int main ()
+{
+	for (
+	unsigned long long i = 1;
+	i <= 0xFFFFFFFF'FFFFFFFFull;
+	i++) {
+		unsigned long long j = i;
+		while(j != 1) {
+			j = n3p1(j);
+		}
+	}
+	
+}
